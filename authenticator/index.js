32e1e7ef5117815d4c9ff177490683b1c76cd626
Andrew Dunkman <adunkman@gmail.com>
2021-04-19 23:11:06 -0400

    Handle requests which do not end in trailing slash

--- authenticator/index.js
+++ authenticator/index.js
@@ -1,13 +1,21 @@
 const credentials = require('./credentials.json');
 
-exports.handler =  async (event) => {
+const appendIndexFile = (url) => {
+  if (!url.pathname.includes('.')) {
+    url.pathname = url.pathname.replace(/\/?$/, '\/index.html');
+  }
+
+  return url.toString();
+};
+
+exports.handler = async (event) => {
   const request = event.Records[0].cf.request;
   const authorization = request.headers.authorization;
   const authString = 'Basic ' + new Buffer(`${credentials.username}:${credentials.password}`).toString('base64');
 
   if (authorization && authorization[0] && authorization[0].value === authString) {
     // Rewrite request URI to append "index.html" if root directory request.
-    request.uri = request.uri.replace(/\/$/, '\/index.html');
+    request.uri = appendIndexFile(new URL(request.url));
 
     // Allow request
     return request;
